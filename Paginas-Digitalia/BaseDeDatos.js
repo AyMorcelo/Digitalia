f2c2a90e941fd87350172be6f2f2c56d2bbf28d6
StevelyMedina <131920192+StevelyMedina@users.noreply.github.com>
2023-05-24 17:43:47 -0400

    Add files via upload

---
+++ Paginas-Digitalia/BaseDeDatos.js
@@ -0,0 +1,29 @@
+fetch('BaseDeDatosDigitalia.json')
+  .then(response => response.json())
+  .then(data => {   
+    // La base de datos se encuentra en el objeto "data"
+    const servicios = data.servicios;
+
+    // Función para agregar un nuevo elemento a la base de datos
+    function agregarNuevoServicio(nuevoServicio) {
+      // Generar un nuevo ID para el servicio
+      const nuevoId = servicios.length > 0 ? servicios[servicios.length - 1].id + 1 : 1;
+      
+      // Agregar el nuevo servicio a la base de datos
+      const servicio = { id: nuevoId, ...nuevoServicio };
+      servicios.push(servicio);
+
+      console.log('Nuevo servicio agregado:', servicio);
+    }
+
+    // Ejemplo de uso: Agregar un nuevo servicio
+    const nuevoServicio = {
+      nombre: 'Servicio de SEO',
+      descripcion: 'Optimización de motores de búsqueda para mejorar la visibilidad en línea.',
+      costo: 2000,
+      duración: '2 meses'
+    };
+
+    agregarNuevoServicio(nuevoServicio);
+  })
+  .catch(error => console.error('Error al leer la base de datos:', error));
